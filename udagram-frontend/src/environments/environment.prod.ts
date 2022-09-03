b6b913dba3e6387628573d3ee4c6b3677d5cc399
Leeefo <a.m.abouelleef@gmail.com>
2022-09-03 19:30:38 +0200

    Fixing some issues

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  // apiHost: "http://localhost:8080/api/v0",
+  apiHost: 'http://udagram-api-dev.eba-rqgi5yn5.us-east-1.elasticbeanstalk.com'
 };
 
 /*
